a0a156864c157dbee9ba041ae2614987a2848f22
memo <memosky@gmail.com>
2021-10-12 19:11:10 -0500

    actualizando el contador

--- index.php
+++ index.php
@@ -10,17 +10,27 @@ $listadoPag = array(); # Array Total de todo el pdf, paggina => peso
 
 $cant_pag = Imagick::getNumPagesPdf($value);
 
+// for($pag = 1 ; $pag <= $cant_pag; $pag++){
+    $archivo = 'A=C:\xampp\htdocs\pdfContador\muestra.pdf';
+//     $archivoSalida = 'A'.$pag;
+//     $pdf_info = exec('C:\xampp\htdocs\pdfContador\pdftk\pdftk.exe '.$archivo.' cat '.$archivoSalida.' output C:\xampp\htdocs\pdfContador\temp\nombre'.$pag.'.pdf');
+
+//     $peso = filesize('C:\xampp\htdocs\pdfContador\temp\nombre'.$pag.'.pdf');
+//     error_log($pag."=>". $peso. ' bytes=>'. ($peso/1024) .' kbytes');
+// }
+
+// $pdf_info = exec('C:\xampp\htdocs\pdfContador\pdftk\pdftk.exe '.$archivo.' dump_data_utf8 ');
+// echo $pdf_info;
+
+$pdf_info = exec('C:\xampp\htdocs\pdfContador\pdftk\pdftk.exe '.$archivo.' burst output temp/pagina%d.pdf');
+
 for($pag = 1 ; $pag <= $cant_pag; $pag++){
-    $archivo = 'A=C:\xampp\htdocs\comprimir\muestra.pdf';
-    $archivoSalida = 'A'.$pag;
-    $pdf_info = exec('C:\xampp\htdocs\comprimir\pdftk\pdftk.exe '.$archivo.' cat '.$archivoSalida.' output C:\xampp\htdocs\comprimir\temp\nombre'.$pag.'.pdf');
-
-    $peso = filesize('C:\xampp\htdocs\comprimir\temp\nombre'.$pag.'.pdf');
-    error_log($pag."=>". $peso. ' bytes=>'. ($peso/1024) .' kbytes');
+    $peso = filesize('temp/pagina'.$pag.'.pdf');
+    $peso = round((($peso/1024)/1024) * 100) / 100;
+    echo 'pagina'.$pag.'.pdf =>'.$peso." megas</br>";
 }
-
 
 // pdftk A=archivo1.pdf cat A1-12 A14-end output salida.pdf
 // $destino = str_replace('//', '/', $_SERVER['DOCUMENT_ROOT']);
-// $FICHERO_PDFTK = $destino.chr(47).'comprimir/pdftk/pdftk.exe';
+// $FICHERO_PDFTK = $destino.chr(47).'pdfContador/pdftk/pdftk.exe';
 
