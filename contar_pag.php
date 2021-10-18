25dbff8093f5b3cf3c4fb259506bbef1e9fecfcb
memo <memosky@gmail.com>
2021-10-18 10:16:52 -0500

    Actualización de split y creacion de pdf

--- contar_pag.php
+++ contar_pag.php
@@ -26,6 +26,34 @@ class Imagick{
             $max; 
         }
 
+    public static   function get_format($df) {
+
+            $str = '';
+            $str .= ($df->invert == 1) ? ' - ' : '';
+         
+            if ($df->y > 0) {
+                // years
+                $str .= ($df->y > 1) ? $df->y . ' Years ' : $df->y . ' Year ';
+            } if ($df->m > 0) {
+                // month
+                $str .= ($df->m > 1) ? $df->m . ' Months ' : $df->m . ' Month ';
+            } if ($df->d > 0) {
+                // days
+                $str .= ($df->d > 1) ? $df->d . ' Days ' : $df->d . ' Day ';
+            } if ($df->h > 0) {
+                // hours
+                $str .= ($df->h > 1) ? $df->h . ' Hours ' : $df->h . ' Hour ';
+            } if ($df->i > 0) {
+                // minutes
+                $str .= ($df->i > 1) ? $df->i . ' Minutes ' : $df->i . ' Minute ';
+            } if ($df->s > 0) {
+                // seconds
+                $str .= ($df->s > 1) ? $df->s . ' Seconds ' : $df->s . ' Second ';
+            } 
+        
+            return $str;
+        }
+
 
     //     function unirArchivos($rutas, $nombre, $ruc , $rutaCarpetaFiscal){
 	// 		$rutaCarpetaFiscalTemp=$rutaCarpetaFiscal."/temp";
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class filePdf
     public $path ;
     public $listadoPag = array();
     public $totalSize ;
-
+    public $totalSeconds ;
 
 }
 
@@ -14,67 +14,55 @@ class filePdf
 
 include_once('contar_pag.php');
 
+
 $rutaTemp = __DIR__."/temp";  // ruta Temporal
-$archivo = "muestra.pdf";     // nombre del pdf
+$archivo = "477527.pdf";     // nombre del pdf
 $value = __DIR__.chr(47).$archivo; # ruta completa archivo original
 $listadoPag = array(); # Array Total de todo el pdf, paggina => peso
 
 $path='C:\xampp2\htdocs\pdfContador';
 $FICHERO_PDFTK='C:\xampp2\htdocs\pdfContador\pdftk\pdftk.exe';
-$sizeFile=1;
+$sizeFile=15;
 $cant_pag = Imagick::getNumPagesPdf($value);
 
-// for($pag = 1 ; $pag <= $cant_pag; $pag++){
-    $archivo = 'A='.$path.chr(47).$archivo;
-    $peso = filesize($path.chr(47).$archivo);
- //   $peso = round((($peso/1024)/1024) * 100) / 100;
-    echo '.pdf =>'.$peso." Bytes</br>";
+$startDate = new DateTime("now");
+
+$peso = filesize($archivo);
+echo '.pdf =>'.$peso." Bytes</br>";
 
 
-//     $archivoSalida = 'A'.$pag;
-//     $pdf_info = exec('C:\xampp\htdocs\pdfContador\pdftk\pdftk.exe '.$archivo.' cat '.$archivoSalida.' output C:\xampp\htdocs\pdfContador\temp\nombre'.$pag.'.pdf');
-
-//     $peso = filesize('C:\xampp\htdocs\pdfContador\temp\nombre'.$pag.'.pdf');
-//     error_log($pag."=>". $peso. ' bytes=>'. ($peso/1024) .' kbytes');
-// }
-
- //$pdf_info = exec('C:\xampp\htdocs\pdfContador\pdftk\pdftk.exe '.$archivo.' dump_data_utf8 ');
- //echo $pdf_info;
-
+$archivo = 'A='.$path.chr(47).$archivo;
+   
 $pdf_info = exec($FICHERO_PDFTK.' '.$archivo.' burst output temp/pagina%d.pdf');
+
+$finalDate = new DateTime("now");
+$diff = $startDate->diff($finalDate);
+echo  "total de segundos :: ".Imagick::get_format($diff).' en realizar el burst';
+
 $totalByte=0;
 
 $listaAll= array();
 
 $filePdf=new filePdf();
-
+$filePdf->totalSize=0;
 
 
 $pdf_info = exec($FICHERO_PDFTK.' @documentos@ cat output @SALIDA_PDF@');
 
-
+$date1 = new DateTime("now");
     for($pag = 1 ; $pag <= $cant_pag; $pag++){
 
-    
-
        
-        $filePdf->listadoPag[]=$pag;
+      
 
         $peso = filesize('temp/pagina'.$pag.'.pdf');
-        $filePdf->totalSize=$filePdf->totalSize+$peso;
 
 
-
-        echo 'pagina'.$pag.'.pdf =>'.$peso." Bytes ";
-        $totalByte+=$peso;
-        $peso = round((($peso/1024)/1024) * 100) / 100;
-        echo ' =>'.$peso." megas</br>";
+        $totalSize=$filePdf->totalSize+$peso;
 
 
-
-
-        if ( $filePdf->totalSize > $sizeFile*1024*1024){
-            
+        if ( $totalSize > $sizeFile*1024*1024){
+         
             $filePdf->path = 'temp/paginaNueva'.count($listaAll).'.pdf';
             $documentos = "A=";
             for($pagu = 0 ; $pagu < count($filePdf->listadoPag); $pagu++){
@@ -89,12 +77,26 @@ $pdf_info = exec($FICHERO_PDFTK.' @documentos@ cat output @SALIDA_PDF@');
                 //$documentos = $documentos.'temp/pagina'.$filePdf->listadoPag[$pagu].'.pdf'.' ';
             }
             
+
+            $date2 = new DateTime("now");
+            $diffAux = $date1->diff($date2);
+            $mili=Imagick::get_format($diffAux);
+            $filePdf->totalSeconds=$mili ;
+           
+            
+           
+           
+            $date1 = new DateTime("now");
             $listaAll[]= $filePdf ;
             $filePdf=new filePdf();
-
+            $filePdf->totalSize=0;
         }
+
+        $filePdf->listadoPag[]=$pag;
+        $filePdf->totalSize=$filePdf->totalSize+$peso;
     }
 
+    //archivos faltantes
     if ( $filePdf->totalSize > 0){
         $filePdf->path = 'temp/paginaNueva'.count($listaAll).'.pdf';
         $documentos = "A=";
@@ -109,12 +111,17 @@ $pdf_info = exec($FICHERO_PDFTK.' @documentos@ cat output @SALIDA_PDF@');
             unlink('temp/pagina'.$filePdf->listadoPag[$pagu].'.pdf');
             //$documentos = $documentos.'temp/pagina'.$filePdf->listadoPag[$pagu].'.pdf'.' ';
         }
+
+        $date2 = new DateTime("now");
+        $diffAux = $date1->diff($date2);
+        $mili=Imagick::get_format($diffAux);
+        $filePdf->totalSeconds=$mili ;
+
         $listaAll[]= $filePdf ;
 
     }
 
 
-echo '.pdf =>'.$totalByte." Bytes</br>";
 
 
 echo '<pre>';
@@ -122,12 +129,10 @@ print_r($listaAll);
 
 echo '</pre>';
 
+echo '</br>';
 
+$finalDate = new DateTime("now");
+$diff = $startDate->diff($finalDate);
+echo  "total de segundos :: ".Imagick::get_format($diff);
 
-
-
-
-// pdftk A=archivo1.pdf cat A1-12 A14-end output salida.pdf
-// $destino = str_replace('//', '/', $_SERVER['DOCUMENT_ROOT']);
-// $FICHERO_PDFTK = $destino.chr(47).'pdfContador/pdftk/pdftk.exe';
 
